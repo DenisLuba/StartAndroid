d12069846324dffbea0be36c74befe37846b1cee
DenisLuba <delubis@mail.ru>
2023-03-22 10:18:55 +0300

    Started P0671_ProgressDialog

---
+++ P0671_ProgressDialog/app/src/main/java/com/example/p0671_progressdialog/MainActivity.java
@@ -0,0 +1,18 @@
+/**
+ * P0671_ProgressDialog
+ */
+
+package com.example.p0671_progressdialog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
