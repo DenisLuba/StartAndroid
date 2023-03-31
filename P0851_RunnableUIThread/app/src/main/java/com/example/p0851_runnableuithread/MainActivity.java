d2b74abcd5f2dcffc95048054452ceb1f2243ef7
DenisLuba <delubis@mail.ru>
2023-03-31 16:43:09 +0300

    Finished P0851_RunnableUIThread

---
+++ P0851_RunnableUIThread/app/src/main/java/com/example/p0851_runnableuithread/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.p0851_runnableuithread;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.concurrent.TimeUnit;
+
+/*
+ * Если алгоритмы не особо сложны,
+ * можно использовать эти методы для выполнения кода в UI-потоке:
+ *
+ * Activity.runOnUiThread(Runnable)
+ * View.post(Runnable)
+ * View.postDelayed(Runnable, long)
+ *
+ * Если же нужны навороты и алгоритм достаточно сложен,
+ * то используем Handler.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String LOG_TAG = "myLogs";
+
+    private TextView tvInfo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvInfo = findViewById(R.id.tvInfo);
+
+        new Thread(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(2);
+                runOnUiThread(() -> tvInfo.setText(R.string.runnable_1));
+                TimeUnit.SECONDS.sleep(2);
+                tvInfo.postDelayed(() -> tvInfo.setText(R.string.runnable_2), 2000);
+                tvInfo.post(() -> tvInfo.setText(R.string.runnable_3));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+}
