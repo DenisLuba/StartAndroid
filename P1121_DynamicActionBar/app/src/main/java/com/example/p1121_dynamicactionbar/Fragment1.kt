6bbf0c3b1289201578449b3569d9339ab1fd1b15
DenisLuba <delubis@mail.ru>
2023-04-19 13:58:57 +0300

    Finished P1121_DynamicActionBar

---
+++ P1121_DynamicActionBar/app/src/main/java/com/example/p1121_dynamicactionbar/Fragment1.kt
@@ -0,0 +1,30 @@
+package com.example.p1121_dynamicactionbar
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class Fragment1 : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setHasOptionsMenu(true)
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment1, container, false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.fragment1, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+}
--- P1121_DynamicActionBar/app/src/main/java/com/example/p1121_dynamicactionbar/MainActivity.kt
+++ P1121_DynamicActionBar/app/src/main/java/com/example/p1121_dynamicactionbar/MainActivity.kt
@@ -2,10 +2,69 @@ package com.example.p1121_dynamicactionbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.DeletedContacts
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.widget.SwitchCompat
+import androidx.fragment.app.Fragment
+import com.example.p1121_dynamicactionbar.databinding.ActivityMainBinding
+import com.google.android.material.switchmaterial.SwitchMaterial
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        @JvmStatic val MENU_ID = 1
+    }
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private lateinit var fragment1: Fragment
+    private lateinit var fragment2: Fragment
+    private lateinit var fragment: Fragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        fragment1 = Fragment1()
+        fragment = fragment1
+        fragment2 = Fragment2()
+
+        with (binding) {
+            switchAddDelete.setOnClickListener(::onClick)
+            switchVisible.setOnClickListener(::onClick)
+            btnFragment.setOnClickListener(::onClick)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        menu?.setGroupVisible(R.id.groupMenu, binding.switchVisible.isChecked)
+        if (binding.switchAddDelete.isChecked) {
+            menu?.add(0, MENU_ID, 0, R.string.menu_item1)
+                ?.setIcon(android.R.drawable.ic_delete)
+                ?.setShowAsAction(
+                    MenuItem.SHOW_AS_ACTION_ALWAYS or
+                            MenuItem.SHOW_AS_ACTION_WITH_TEXT
+                )
+        } else menu?.removeItem(MENU_ID)
+
+        return true
+    }
+
+    private fun onClick(view: View) {
+        when (view.id) {
+            R.id.switchAdd_Delete, R.id.switchVisible -> invalidateOptionsMenu() // перерисовка меню/ActionBar
+            R.id.btnFragment -> {
+                fragment = if (fragment == fragment1) fragment2 else fragment1
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.content, fragment)
+                    .commit()
+            }
+        }
     }
 }
