e3f32d11912b410ae6c437ea4ffdb79a56dc8f67
DenisLuba <delubis@mail.ru>
2023-04-23 13:59:20 +0300

    Finished P1191_PendingIntent

--- P1191_PendingIntent/app/src/main/java/com/example/p1191_pendingintent/MainActivity.kt
+++ P1191_PendingIntent/app/src/main/java/com/example/p1191_pendingintent/MainActivity.kt
@@ -7,9 +7,11 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
 import android.view.View
 import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
@@ -17,6 +19,31 @@ import androidx.core.app.NotificationCompat.Builder
 import androidx.core.app.NotificationManagerCompat
 import com.example.p1191_pendingintent.Receiver.Companion.log
 import com.example.p1191_pendingintent.databinding.ActivityMainBinding
+
+/**
+ * Флаг изменяемоси должен быть обязательно:
+ *
+ * FLAG_MUTABLE - PendingIntent должен быть изменяемым.
+ *
+ * FLAG_IMMUTABLE - Флаг, указывающий, что созданный объект PendingIntent должен быть неизменяемым.
+ * Это означает, что дополнительный аргумент Intent, передаваемый методам отправки (sendNotification, например)
+ * для заполнения незаполненных свойств этого Intent, будет игнорироваться.
+ *
+ * С этими флагами могут применяться дополнительные флаги:
+ *
+ * FLAG_NO_CREATE - PendingIntent не будет создан, если среди существующих нет похожего.
+ *
+ * FLAG_ONE_SHOT - PendingIntent с этим флагом сработает лишь один раз.
+ *
+ * FLAG_CANCEL_CURRENT - Если система видит, что создаваемый с таким флагом PendingIntent похож на существующий,
+ * то она отменит (удалит) существующий.
+ *
+ * FLAG_UPDATE_CURRENT - Флаг, противоположный флагу FLAG_CANCEL_CURRENT.Если система видит,
+ * что создаваемый с таким флагом PendingIntent похож на существующий,
+ * то она возьмет extra-данные Intent создаваемого PendingIntent и запишет их вместо extra-данных Intent существующего PendingIntent.
+ * Проще говоря, существующий PendingIntent будет использовать Intent из создаваемого.
+ *
+ */
 
 class MainActivity : AppCompatActivity() {
 
@@ -32,10 +59,10 @@ class MainActivity : AppCompatActivity() {
         getSystemService(ALARM_SERVICE) as AlarmManager
     }
 
-    private lateinit var intent1: Intent
-    private lateinit var intent2: Intent
-    private lateinit var pendingIntent1: PendingIntent
-    private lateinit var pendingIntent2: PendingIntent
+    private var intent1: Intent? = null
+    private var intent2: Intent? = null
+    private var pendingIntent1: PendingIntent? = null
+    private var pendingIntent2: PendingIntent? = null
 
     companion object {
         private const val CHANNEL_ID = "myChannel"
@@ -58,25 +85,25 @@ class MainActivity : AppCompatActivity() {
                 pendingIntent1 = PendingIntent.getBroadcast(
                     this,
                     0,
-                    intent1,
-                    PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+                    intent1!!,
+                    PendingIntent.FLAG_MUTABLE
                 )
 
                 intent2 = createIntent("action", "extra 2")
                 pendingIntent2 = PendingIntent.getBroadcast(
                     this,
                     0,
-                    intent2,
-                    PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+                    intent2!!,
+                    PendingIntent.FLAG_MUTABLE
                 )
 
-                compare()
+                log("start")
+                alarmManager.set(AlarmManager.RTC, System.currentTimeMillis() + 2000, pendingIntent1)
+                alarmManager.set(AlarmManager.RTC, System.currentTimeMillis() + 4000, pendingIntent2)
 
-                sendNotification(1, pendingIntent1)
-                sendNotification(2, pendingIntent2)
             }
             binding.button2.id -> {
-
+                alarmManager.cancel(pendingIntent2)
             }
         }
     }
@@ -88,7 +115,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun compare() {
-        log("intent1 == intent2: ${ intent1.filterEquals(intent2) }")
+        log("intent1 == intent2: ${ intent1?.filterEquals(intent2) }")
         log("pendingIntent1 == pendingIntent2: ${ pendingIntent1 == pendingIntent2 }")
     }
 
