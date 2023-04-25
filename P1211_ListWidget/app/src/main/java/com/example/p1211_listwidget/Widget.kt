08bdcd058c30bcc3e944eb4858499a6b493e0025
DenisLuba <delubis@mail.ru>
2023-04-25 16:40:51 +0300

    finished 122 lesson

--- P1211_ListWidget/app/src/main/java/com/example/p1211_listwidget/Widget.kt
+++ P1211_ListWidget/app/src/main/java/com/example/p1211_listwidget/Widget.kt
@@ -1,13 +1,17 @@
 package com.example.p1211_listwidget
 
+import android.app.AlarmManager
 import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
+import android.appwidget.AppWidgetProviderInfo
+import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.icu.text.SimpleDateFormat
 import android.icu.util.TimeZone
 import android.net.Uri
+import android.os.Bundle
 import android.util.Log
 import android.widget.RemoteViews
 import android.widget.Toast
@@ -21,6 +25,7 @@ class Widget : AppWidgetProvider() {
 
         const val ACTION_ON_CLICK = "com.example.p1211_listwidget.itemonclick"
         const val ITEM_POSITION = "item_position"
+        const val UPDATE_ALL_WIDGETS = "update_all_widgets"
 
         @JvmStatic fun getTime(): String =
             SimpleDateFormat("HH:mm:ss", Locale.getDefault()).run {
@@ -31,26 +36,109 @@ class Widget : AppWidgetProvider() {
         @JvmStatic fun log(message: String) = Log.d("myLogs${this::class.simpleName}", message)
     }
 
+//      вызывается в ответ на широковещательное сообщение AppWidgetManager#ACTION_APPWIDGET_ENABLED
+//      при создании экземпляра виджета
+    override fun onEnabled(context: Context?) {
+        super.onEnabled(context)
+        log("Widget: onEnabled")
+        val intent = Intent(context, Widget::class.java).apply {
+            action = UPDATE_ALL_WIDGETS
+        }
+        val pendingIntent = PendingIntent.getBroadcast(
+            context,
+            0,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+        val alarmManager: AlarmManager = context?.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        alarmManager.setRepeating(AlarmManager.RTC, System.currentTimeMillis(), 60000, pendingIntent)
+    }
+//      вызывается в ответ на широковещательное сообщение AppWidgetManager#ACTION_APPWIDGET_DISABLED
+//      при удалении последнего экземпляра виджета
+    override fun onDisabled(context: Context?) {
+        super.onDisabled(context)
+        log("Widget: onDisabled")
+        val intent = Intent(context, Widget::class.java).apply {
+            action = UPDATE_ALL_WIDGETS
+        }
+        val pendingIntent = PendingIntent.getBroadcast(
+            context,
+            0,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+        val alarmManager: AlarmManager = context?.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        alarmManager.cancel(pendingIntent)
+    }
+
+//    при изменении размера виджета
+    override fun onAppWidgetOptionsChanged(
+        context: Context?,
+        appWidgetManager: AppWidgetManager?,
+        appWidgetId: Int,
+        newOptions: Bundle?
+    ) {
+        super.onAppWidgetOptionsChanged(context, appWidgetManager, appWidgetId, newOptions)
+
+        if (newOptions == null || context == null) return
+        val minWidth: Int = newOptions.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH, 0)
+        val maxWidth: Int = newOptions.getInt(AppWidgetManager.OPTION_APPWIDGET_MAX_WIDTH, 0)
+        val minHeight: Int = newOptions.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_HEIGHT, 0)
+        val maxHeight: Int = newOptions.getInt(AppWidgetManager.OPTION_APPWIDGET_MAX_HEIGHT, 0)
+
+        val widgetHostCategory: Int = newOptions.getInt(AppWidgetManager.OPTION_APPWIDGET_HOST_CATEGORY, 0)
+
+        Toast.makeText(context,
+            """
+            Width : $minWidth - $maxWidth
+            Height : $minHeight - $maxHeight
+            Category - ${
+                when (widgetHostCategory) {
+                    AppWidgetProviderInfo.WIDGET_CATEGORY_KEYGUARD -> "Keyguard"
+                    AppWidgetProviderInfo.WIDGET_CATEGORY_HOME_SCREEN -> "Home Screen"
+                    else -> "none"
+                }
+            }
+            """.trimIndent(),
+            Toast.LENGTH_LONG)
+            .show()
+    }
+
+//    Метод приемника (ресивера)
     override fun onReceive(context: Context?, intent: Intent?) {
         super.onReceive(context, intent)
 
         if (context == null || intent == null) return
-        log("Widget: onReceive")
-        when (intent.action) {
+        val action: String? = intent.action
+        log("Widget: onReceive. ACTION = $action")
+
+        when (action) {
             AppWidgetManager.ACTION_APPWIDGET_UPDATE -> {
                 val widgetID: Int = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, -1)
+//                получение AppWidgetManager с помощью Context
                 val appWidgetManager: AppWidgetManager = AppWidgetManager.getInstance(context)
                 updateWidget(context, appWidgetManager, widgetID)
             }
             ACTION_ON_CLICK -> {
-                log("Widget: onReceive. item click")
                 val itemPosition: Int = intent.getIntExtra(ITEM_POSITION, -1)
                 if (itemPosition != -1)
                     Toast.makeText(context, "Clicked on the item $itemPosition", Toast.LENGTH_SHORT).show()
             }
+            UPDATE_ALL_WIDGETS -> {
+                val thisAppWidget = ComponentName(
+                    context.packageName,
+                    this::class.java.name
+                )
+                val appWidgetManager: AppWidgetManager = AppWidgetManager.getInstance(context)
+                val widgetIDs: IntArray = appWidgetManager.getAppWidgetIds(thisAppWidget)
+                for (widgetID in widgetIDs) {
+                    updateWidget(context, appWidgetManager, widgetID)
+                }
+            }
         }
     }
 
+//    при создании и при обновлении (хз, почему-то не срабатывает при обновлении)
     override fun onUpdate(
         context: Context,
         appWidgetManager: AppWidgetManager,
@@ -87,7 +175,7 @@ class Widget : AppWidgetProvider() {
             context,
             widgetID,
             updateIntent,
-            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_MUTABLE
         )
         remoteViews.setOnClickPendingIntent(R.id.tvUpdate, updatePending)
     }
@@ -109,7 +197,10 @@ class Widget : AppWidgetProvider() {
             context,
             0,
             listClickIntent,
-            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_MUTABLE
+//            флаг должен быть MUTABLE,
+//            чтобы его одного можно было применять к разным интентам в разных
+//            пунктах в списке
+            PendingIntent.FLAG_MUTABLE
         )
         remoteViews.setPendingIntentTemplate(R.id.listView, listClickPendingIntent)
     }
